f0994141ceb0d1d1408bad653396a3608cbb127a
Wangzhonghua <1138801254@qq.com>
2019-01-25 22:32:33 +0800

    The protice test01 - First time

---
+++ Scripts/test_1.py
@@ -0,0 +1,10 @@
+
+class Test_01:
+
+    def test_01_001(self):
+        print("-->test_01_001")
+        assert False
+
+    def test_01_002(self):
+        print("-->test_01_002")
+        assert True
